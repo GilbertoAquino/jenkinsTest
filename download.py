bdbcedcf894feca6be7eac0bb3ddfb86a93ca76e
Gilberto Aquino <gaquinos@outlook.com>
2023-04-21 13:27:16 -0600

    workaround from raspishake client

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 import argparse
 from obspy.clients.fdsn import Client
 from obspy import UTCDateTime, read_inventory, read
+from obspy import read
+from subprocess import run
 import os
 
 parser = argparse.ArgumentParser()
@@ -10,14 +12,22 @@ parser.add_argument("--time",type=str)
 parser.add_argument("--delta",type=str)
 args=parser.parse_args()
 print(args)
-rs = Client('RASPISHAKE')
-st= UTCDateTime(args.date+"T"+args.time)
-if args.delta[3][len(args.delta[3])-1].lower() == 'h':
-    end=st+60*60*float(args.delta[3][1:len(args.delta[3])-1])
-elif args.delta[3][len(args.delta[3])-1].lower() == 'm':
-    end=st+60*float(args.delta[3][1:len(args.delta[3])-1])
-elif args.delta[3][len(args.delta[3])-1].lower() == 's':
-    end=st+float(args.delta[3][1:len(args.delta[3])-1])
-inv = rs.get_stations(station=args.station, sttime=st, endtime=end)
-stream = rs.get_waveforms('AM', args.station, '00', '*', st, end)
+st = UTCDateTime(args.date+"T"+args.time)
+sdt = st.datetime.strftime("%Y-%m-%dT%H:%M:%S")
+if args.delta[-1].lower() == 'h':
+    end=st+60*60*float(args.delta[:len(args.delta)-1])
+elif args.delta[-1].lower() == 'm':
+    end=st+60*float(args.delta[:len(args.delta)-1])
+elif args.delta[-1].lower() == 's':
+    end=st+float(args.delta[:len(args.delta)-1])
+end = end.datetime.strftime("%Y-%m-%dT%H:%M:%S")
+rs = f"'https://data.raspberryshake.org/fdsnws/dataselect/1/query?net=AM&sta={args.station}&loc=00&cha=*&start={st}&end={end}'"
+try:
+    run(["wget", rs,"-O", "data.mseed"],check=True)
+except:
+    run(["powershell","wget", rs,"-O", "data.mseed"],check=True)
+stream = read("data.mseed")
 stream.plot()
+#inv = rs.get_stations(station=args.station, sttime=st, endtime=end)
+#stream = rs.get_waveforms('AM', args.station, '00', '*', st, end)
+#stream.plot()
